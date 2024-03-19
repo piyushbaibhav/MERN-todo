55b7bb32318d2f043cd0eac9e21b4e06e1ba02b3
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-03-19 23:41:13 +0530

    correct endpoints

--- server/routes.js
+++ server/routes.js
@@ -9,25 +9,29 @@ router.get("/healthcheck", (req, res) => {
 
 
 //Get /todos
-router.get("/todos",(req,res)=>{
+router.get("/tasks",(req,res)=>{
     res.status(200).json({msg: "GET REQUEST to /api/todos"})
+});
+//Get specific /todos
+router.get("/tasks/:id",(req,res)=>{
+    res.status(200).json({msg: "GET REQUEST to /api/todos/:id"})
 });
 
 //Post /todos
-router.post("/todos", (req, res) => {
+router.post("/post", (req, res) => {
   res.status(201).json({ msg: "POST REQUEST to /api/todos" });
 });
 
 
 //Delete /todos/:id
-router.delete("/todos/:id", (req, res) => {
+router.delete("/post/:id", (req, res) => {
   res.status(200).json({ msg: "DELETE REQUEST to /api/todos/:id" });
 });
 
 
 
 //Put /todos/:id
-router.put("/todos/:id", (req, res) => {
+router.put("/post/:id", (req, res) => {
   res.status(200).json({ msg: "UPDATE REQUEST to /api/todos/:id" });
 });
 
